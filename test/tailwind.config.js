d4668f87239ff81fcae0b42249d10a3e8d65ca31
meriem <meriem.mghirbi@esprit.tn>
2025-03-05 23:26:16 +0100

    first commit

---
+++ test/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./*.{html,scss}"], // Vérifie tous les fichiers HTML et SCSS
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
