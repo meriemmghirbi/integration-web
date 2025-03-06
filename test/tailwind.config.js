fd905a1d50bbc79f5b7f481016bce6271da74400
AZIZhalleb <Mouhamedaziz.HALLEB@esprit.tn>
2025-03-07 00:13:09 +1200

    aziz

--- test/tailwind.config.js
+++ test/tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./*.{html,scss}"], // Vérifie tous les fichiers HTML et SCSS
+  content: ["./index.html", "./*.{html,scss}", "./public/**/*.{html,js}", "./src/**/*.{html,js}"], // Vérifie tous les fichiers HTML et SCSS
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#137C8B",
+        secondary: "#7A90A4",
+        tertiary: "#6e757a",
+      },
+      fontFamily: {
+        body: ["Work Sans"],
+      },
+      fontSize: {
+        navSize: "24px",
+        heroSize: "74px",
+        pSize: "18px",
+      },
+    },
   },
   plugins: [],
 };
